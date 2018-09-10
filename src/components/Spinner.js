f41880f79a686a490082e89a3831a717e5f45361
Martin Brännstrand <martin.brannstrand@gmail.com>
2018-09-10 22:40:18 +0200

    Style spinner with utils

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Spinner = () => {
   return (
-    <div className="spinner">
+    <div className="bg-beige fixed flex items-center justify-center pin spinner">
       <div className="spinner__bounce-1" />
       <div className="spinner__bounce-2" />
       <div className="spinner__bounce-3" />
